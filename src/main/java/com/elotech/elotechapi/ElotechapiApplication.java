24ed51c2a30d98292da9e2719cbd3affefea4176
AdlerCachuba <adlercachuba@gmail.com>
2024-04-06 12:58:55 -0300

    feat: init project

---
+++ src/main/java/com/elotech/elotechapi/ElotechapiApplication.java
@@ -0,0 +1,13 @@
+package com.elotech.elotechapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElotechapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElotechapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elotech/elotechapi/ElotechapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elotech.elotechapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElotechapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
